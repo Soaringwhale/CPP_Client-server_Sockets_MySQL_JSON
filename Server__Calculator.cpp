72e9c90121d63653d2644f0b5324a65067a43ab7
Refrain <69113717+Soaringwhale@users.noreply.github.com>
2022-03-19 19:40:40 +0300

    Add files via upload

---
+++ Server__Calculator.cpp
@@ -0,0 +1,49 @@
+#include "Server__Calculator.h"
+
+ServerCalculator::ServerCalculator()
+{
+
+}
+
+ServerCalculator::~ServerCalculator()
+{
+
+}
+
+
+
+Complx ServerCalculator::summOfCmplx(float real1, float im1, float real2, float im2)
+{
+	a.real = real1;
+	a.im = im1;
+	b.real = real2;
+	b.im = im2;
+	return a + b;
+}
+
+Complx ServerCalculator::differenceOfCmplx(float real1, float im1, float real2, float im2)
+{
+	a.real = real1;
+	a.im = im1;
+	b.real = real2;
+	b.im = im2;
+	return a - b;
+}
+
+Complx ServerCalculator::multOfCmplx(float real1, float im1, float real2, float im2)
+{
+	a.real = real1;
+	a.im = im1;
+	b.real = real2;
+	b.im = im2;
+	return a * b;
+}
+
+Complx ServerCalculator::divOfCmplx(float real1, float im1, float real2, float im2)
+{
+	a.real = real1;
+	a.im = im1;
+	b.real = real2;
+	b.im = im2;
+	return a / b;
+}
